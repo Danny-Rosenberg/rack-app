045ed115e64d3224cf8e06d04cfb4321af99231f
Daniel Rosenberg <rosenbergd36@gmail.com>
2022-01-24 22:14:17 -0500

    add first draft of counter

--- app/config.ru
+++ app/config.ru
@@ -1,6 +1,8 @@
 require_relative './logging_middleware'
+require_relative './hit_counter'
 require_relative './rack_app'
 
 use LoggingMiddleware
+use HitCounterMiddleware
 
 run RackApp.new
--- app/hit_counter.rb
+++ app/hit_counter.rb
@@ -1,5 +1,52 @@
 class HitCounterMiddleware
 
+	INTERVAL = 300
 
+	@@hits  = Array.new(INTERVAL, 0)
+	@@times = Array.new(INTERVAL, 0)
+
+	attr_reader :now
+
+	def initialize(app)
+    @app = app
+		@now = Time.now.to_i
+  end
+
+  def call(env)
+		record_hit(INTERVAL)
+		status, headers, body = @app.call(env)
+		# is this using the same 'now'?
+		hits = get_hits(INTERVAL)
+		[status, headers, body << "hits in the last five minutes #{hits}:" ]
+	end
+
+
+private
+
+
+	def record_hit(interval = INTERVAL)
+		index = now % interval
+		# 'bust the cache' if necessary
+		unless now == @@times[index]
+			@@times[index] = now
+			@@hits[index] = 1
+		else
+			# we have some simultaneous hits in the current second
+			@@hits[index] += 1
+		end
+	end
+
+
+	# default to 5 minutes
+	def get_hits(seconds = INTERVAL)
+		hit_count = 0
+		@@times.each_with_index do |time, index|
+			# if the value in the times array is within the interval, add it to the count
+			if now - time  < seconds
+				hit_count += @@hits[index]
+			end
+		end
+		hit_count
+	end
 
 end
