82cf1c18c3f384a6ee0ba8ca3eba07bdcfdfe2af
Daniel Rosenberg <rosenbergd36@gmail.com>
2022-01-24 22:13:26 -0500

    add some convenience methods and loading to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,15 @@
 require 'rack/test'
 require 'pry'
+require 'timecop'
 
 include Rack::Test::Methods
+
+path = File.join(File.dirname(__FILE__), '../app/config.ru')
+OUTER_APP = Rack::Builder.parse_file(path).first
+
+def app
+	OUTER_APP
+end
+
+# require all the ruby file in the app
+Dir[File.join(__dir__, '../app', '*.rb')].each { |file| require file }
